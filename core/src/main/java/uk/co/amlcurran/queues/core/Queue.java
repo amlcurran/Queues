3e2e7fa7e780ae3861f42d14593116e0121abdff
Alex Curran <aml.curran@gmail.com>
2015-02-20 18:27:02 +0000

    Single item in a queue

--- core/src/main/java/uk/co/amlcurran/queues/core/Queue.java
+++ core/src/main/java/uk/co/amlcurran/queues/core/Queue.java
@@ -3,17 +3,22 @@ package uk.co.amlcurran.queues.core;
 public class Queue {
     private final QueuePersister queuePersister;
     private long id;
+    private QueueItem queueItem;
 
     public Queue(QueuePersister queuePersister) {
-
         this.queuePersister = queuePersister;
     }
 
     public void addItem(QueueItem queueItem) {
+        this.queueItem = queueItem;
         queuePersister.addItemToQueue(id, queueItem);
     }
 
     public long getId() {
         return id;
+    }
+
+    public QueueItem next() {
+        return queueItem;
     }
 }
--- core/src/test/java/uk/co/amlcurran/queues/core/QueueTest.java
+++ core/src/test/java/uk/co/amlcurran/queues/core/QueueTest.java
@@ -19,6 +19,24 @@ public class QueueTest {
         assertThat(persister.saveNewItem_queueId, is(queue.getId()));
     }
 
+    @Test
+      public void testAddingAnItemToAQueue_MeansItCanBeRetrieved() {
+        Queue queue = new Queue(UNUSED_PERSISTER);
+
+        QueueItem queueItem = new QueueItem("Hello");
+        queue.addItem(queueItem);
+
+        assertThat(queue.next(), is(queueItem));
+    }
+
+    private static final QueuePersister UNUSED_PERSISTER = new QueuePersister() {
+        @Override
+        public void addItemToQueue(long queueId, QueueItem queueItem) {
+
+        }
+    };
+
+
     private class AssertingQueuePersister implements QueuePersister {
         public QueueItem saveNewItem_item;
         public long saveNewItem_queueId;
