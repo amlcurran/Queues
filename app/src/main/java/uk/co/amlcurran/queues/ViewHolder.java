a4a3f0e0965b3abc96841a468b207eb1da3e0904
Alex Curran <aml.curran@gmail.com>
2015-03-25 07:52:27 +0000

    Reduce class visibility

--- app/src/main/java/uk/co/amlcurran/queues/ViewHolder.java
+++ app/src/main/java/uk/co/amlcurran/queues/ViewHolder.java
@@ -3,7 +3,7 @@ package uk.co.amlcurran.queues;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class ViewHolder extends RecyclerView.ViewHolder {
+class ViewHolder extends RecyclerView.ViewHolder {
 
     public ViewHolder(View itemView) {
         super(itemView);
