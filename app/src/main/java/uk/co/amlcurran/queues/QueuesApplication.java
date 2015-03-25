5432dd9e6a69c50842b92f4b8c084627178598f0
Alex Curran <aml.curran@gmail.com>
2015-03-25 07:35:34 +0000

    Add non-static singleton queuelist

---
+++ app/src/main/java/uk/co/amlcurran/queues/QueuesApplication.java
@@ -0,0 +1,56 @@
+package uk.co.amlcurran.queues;
+
+import android.app.Application;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import uk.co.amlcurran.queues.core.Queue;
+import uk.co.amlcurran.queues.core.QueueItem;
+import uk.co.amlcurran.queues.core.QueueList;
+import uk.co.amlcurran.queues.core.QueuePersister;
+
+public class QueuesApplication extends Application {
+
+    private QueueList queueList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        queueList = new QueueList(new QueuePersister() {
+
+            private final List<Queue> queues = new ArrayList<>();
+
+            @Override
+            public void addItemToQueue(final long queueId, final QueueItem queueItem) {
+            }
+
+            @Override
+            public void removeItemFromQueue(final long queueId, final QueueItem queueItem) {
+
+            }
+
+            @Override
+            public List<Queue> queues() {
+                return Collections.unmodifiableList(queues);
+            }
+
+            @Override
+            public void saveQueue(final Queue queue, final Callbacks callbacks) {
+                queues.add(queue);
+            }
+
+            @Override
+            public long uniqueId() {
+                return queues.size();
+            }
+        });
+    }
+
+    public static QueueList queueList(Context context) {
+        return ((QueuesApplication) context.getApplicationContext()).queueList;
+    }
+
+}
