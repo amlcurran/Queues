2020951f4d4b60f4765d8db3a1c83fc04387514f
Alex Curran <aml.curran@gmail.com>
2015-04-11 13:38:06 +0100

    Remove unused menu items

--- app/src/main/java/uk/co/amlcurran/queues/list/QueueListActivity.java
+++ app/src/main/java/uk/co/amlcurran/queues/list/QueueListActivity.java
@@ -22,28 +22,4 @@ public class QueueListActivity extends ActionBarActivity {
                 .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        final boolean createOptionsMenu = super.onCreateOptionsMenu(menu);
-        menu.add("Add");
-        menu.add("Remove");
-        return createOptionsMenu;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        if ("Remove".equals(item.getTitle())) {
-            removeQueue();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void removeQueue() {
-        QueueList queueList = QueuesApplication.queueList(this);
-        if (queueList.size() > 0) {
-            Queue queue = queueList.all().get(0);
-            queueList.remove(queue);
-        }
-    }
-
 }
