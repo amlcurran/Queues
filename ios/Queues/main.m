7658de6f0f21f62327e44fb7a1d29594af64ef43
Alex Curran <aml.curran@gmail.com>
2015-04-28 22:16:58 +0100

    Commit basic iOS build

---
+++ ios/Queues/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Queues
+//
+//  Created by Alex on 28/04/2015.
+//  Copyright (c) 2015 Alex Curran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
